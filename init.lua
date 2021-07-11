e478e4a90206cd11853a49d2a33402ccf02ea9ff
tamton-aquib <aquibjavedt007@gmail.com>
2021-07-11 21:01:22 +0530

    nice arrows for sections

--- init.lua
+++ init.lua
@@ -1,17 +1,17 @@
--- GENERAL SETTINGS AND MAPPINGS
+--> GENERAL SETTINGS AND MAPPINGS
 require 'general.settings'
 require 'general.mappings'
 require 'general.functions'
 require 'general.packer'
 
--- THEMES AND STATUSLINE
+--> THEMES AND STATUSLINE
 -- require 'themes.sonokai' --> sonokai, tokyonight
 require 'themes.staline'
 vim.opt.rtp:append '~/TOOLS/noice_dark'
 vim.g.noice_transparency = "none"
 vim.g.colors_name = "noice"  --> onedark, gruvbox, tokyodark
 
--- PLUG CONFIGS
+--> PLUG CONFIGS
 require 'config.one-liner-configs'
 require 'config.snippets'
 require 'config.plug-colorizer'
@@ -23,7 +23,7 @@ require 'config.floaterm'
 require 'config.neorg'
 require 'config.lirtree'
 
--- LSP SETTINGS
+--> LSP SETTINGS
 require 'lsp.lspconfig'
 require 'lsp.compe-config'
 require 'lsp.lsps'
