583b9fa370d4a1640eff2dae2089806da877f96d
tamton-aquib <aquibjavedt007@gmail.com>
2021-07-20 15:20:58 +0530

    packer update

--- lua/general/packer.lua
+++ lua/general/packer.lua
@@ -11,12 +11,8 @@ packer.init {
 }
 
 packer.startup { function(use)
--- 	use '/home/taj/TOOLS/noiceboard'
 
--- 	use 'ghifarit53/tokyonight-vim'
-	use 'folke/zen-mode.nvim'
 	use 'tamton-aquib/staline.nvim'
-	use 'kyazdani42/nvim-tree.lua'
 
 	--> Themes and UI
 	use 'ghifarit53/tokyonight-vim'
@@ -33,14 +29,6 @@ packer.startup { function(use)
 	use 'hrsh7th/nvim-compe'
 	use 'kabouzeid/nvim-lspinstall'
 	-- use {'onsails/lspkind-nvim', config = function() require'lspkind'.init{with_text=false} end}
-
-	--> General Purpose
-	use 'L3MON4D3/LuaSnip'
-	use 'kyazdani42/nvim-web-devicons'
--- 	use 'tamago324/lir.nvim'
-	use 'steelsojka/pears.nvim'
-	use 'akinsho/nvim-toggleterm.lua'
-	use {'vhyrro/neorg', branch =  'unstable'}
 
 	--> Web dev Utils
 	use {'iamcco/markdown-preview.nvim', ft={'markdown'}}
