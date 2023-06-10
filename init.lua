c11a2c7e99c3de0f0e710a463afc468b8ff506b5
Aquib Javed <77913442+tamton-aquib@users.noreply.github.com>
2023-06-10 23:50:57 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -264,6 +264,9 @@ map('n', '<leader>cc', Util.chatgpt)
 map('n', '<leader>hn', cmd "Gitsigns next_hunk")
 map('n', '<leader>hp', cmd "Gitsigns prev_hunk")
 map('n', 'gpd', function() require("goto-preview").goto_preview_definition({}) end)
+map('n', '<leader>ii', function() require("lazyn").install_picker() end)
+map('n', '<leader>iu', function() require("lazyn").remove_picker() end)
+
 
 map('n', '<C-n>', cmd "cnext")
 map('n', '<C-p>', cmd "cprev")
@@ -472,18 +475,12 @@ local path = vim.fn.stdpath("config") .. "/plugins.json"
 local real_plugins = {
     -->  Temporary and testing
     { 'edluffy/hologram.nvim', config=true, lazy=true },
-    { 'wilmanbarrios/palenight.nvim' },
-    { 'folke/edgy.nvim', config=true },
-    -- { 'maxmx03/fluoromachine.nvim', config={ glow = true, transparent="full" } },
-    -- { 'EdenEast/nightfox.nvim' },
-    -- { 'gorbit99/codewindow.nvim', config=true },
+    { 'rmagatti/goto-preview', opts={border=Util.border} },
     -- { 'Saecki/crates.nvim', event={'BufRead Cargo.toml'}, config=true },
-    { 'simrat39/rust-tools.nvim', config=true },
-    -- { 'Maan2003/lsp_lines.nvim', config=true, event='LspAttach' },
     -- { 'kylechui/nvim-surround', config=true }
 
     -->  My Useless lil plugins
-    -- { 'tamton-aquib/flirt.nvim', config=true },
+    { 'tamton-aquib/flirt.nvim', config=true },
     { 'tamton-aquib/stuff.nvim' },
     { 'tamton-aquib/essentials.nvim', lazy=true },
 
@@ -636,100 +633,4 @@ vim.api.nvim_create_autocmd("BufReadPost", {
         })
     end
 })
--- }}}
-
--- {{{ -- Plug Installer
-
--- TODO: too ugly, cant change rn
-local gline, buf
-local plugin_list = {}
-local curl = require("plenary.curl")
-local json_path = vim.fn.stdpath("config") .. "/plugins.json"
-
--- NOTE: Kinda inefficient. Too lazy to do the normal way :(
-local actions = {
-    get = function() return vim.json.decode(table.concat(vim.fn.readfile(json_path))) end,
-
-    set = function(p)
-        vim.fn.writefile({vim.fn.json_encode(p)}, json_path)
-        vim.notify("Wrote changes to plugins.json!")
-    end
-}
-
-local remove_selected_plugin = function()
-    local line = vim.trim(vim.api.nvim_get_current_line())
-    local ps = actions.get()
-    if not ps then return end
-
-    local newt = {}
-    for _,p in ipairs(ps) do
-        if not p.url:match(vim.pesc(line)) then
-            table.insert(newt, p)
-        end
-    end
-    actions.set(newt)
-    vim.api.nvim_del_current_line()
-end
-local install_selected_plugin = function()
-    local result = vim.trim(vim.api.nvim_get_current_line())
-    local selected = plugin_list[result].full_name
-
-    local ps = actions.get()
-    if not ps then return end
-    table.insert(ps, {url=[[https://github.com/]]..selected, config=true})
-    actions.set(ps)
-end
-
-local update_lines = function(d)
-    plugin_list = vim.json.decode(d)
-    vim.schedule(function()
-        vim.api.nvim_buf_set_lines(buf, 1, -1, false, {})
-        local t = vim.tbl_keys(plugin_list)
-        vim.api.nvim_buf_set_lines(buf, 1, 2, false, t)
-    end)
-end
-
-local search_plugins = function()
-    local line = vim.api.nvim_buf_get_lines(buf, 0, 1, true)[1]
-
-    if vim.bo.ft ~= "lazy_search" or line:len() < 3 or line == gline then
-        return
-    end
-
-    curl.get("https://api.nvimplugnplay.repl.co/search?max_count=10&query="..line, {
-        callback=function(d) update_lines(d.body) end
-    })
-
-    gline = line
-end
-
-local picker_base = function()
-    buf = vim.api.nvim_create_buf(false, true)
-    vim.cmd [[leftabove vsplit | vert resize 30]]
-    vim.api.nvim_set_current_buf(buf)
-    vim.cmd [[set nonu nornu ft=lazy_search]]
-    require("essentials.utils").set_quit_maps()
-end
-local install_picker = function()
-    picker_base()
-    vim.keymap.set({"i", "n"}, "<CR>", install_selected_plugin, { buffer=buf })
-    vim.api.nvim_create_autocmd('CursorHoldI', { callback=search_plugins })
-end
-local remove_picker = function()
-    picker_base()
-    local ps = actions.get()
-    if not ps then return end
-
-    local newt = {}
-    for _, p in ipairs(ps) do
-        local json_bruh = p.url:gsub("https://github.com/", "")
-        table.insert(newt, json_bruh)
-    end
-    vim.api.nvim_put(newt, "", false, false)
-    vim.keymap.set({"i", "n"}, "<CR>", remove_selected_plugin, { buffer=buf })
-end
-
-vim.keymap.set('n', '<leader>ii', install_picker, {})
-vim.keymap.set('n', '<leader>iu', remove_picker, {})
-
 -- }}}
